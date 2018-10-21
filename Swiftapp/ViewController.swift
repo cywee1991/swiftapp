ded4064c48123ae6abd60d03dff39a3c5ea343b8
Chan Yan wee <cywee1991@gmail.com>
2018-10-21 21:41:30 +0800

    Initial Commit

---
+++ Swiftapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swiftapp
+//
+//  Created by Chan Yan wee on 21/10/18.
+//  Copyright © 2018 Chan Yan wee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
